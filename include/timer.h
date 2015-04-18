3b4d7837da15b0a3fa23e0b7f3ee8e04d82bf26d
Morain <morain@morain.(none)>
2015-04-18 15:12:57 +0800

    加入中断机制和时钟中断

---
+++ include/timer.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timer.h
+ *
+ *    Description:  时钟中断机制
+ *
+ *        Version:  1.0
+ *        Created:  2015年04月17日 21时27分52秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Morain 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_TIMER_H_
+#define INCLUDE_TIMER_H_
+
+#include "types.h"
+
+void init_timer(uint32_t frequency);
+
+#endif
--- init/entry.c
+++ init/entry.c
@@ -17,13 +17,15 @@
  */
 #include "console.h"
 #include "gdt.h"
-#include "string.h"
 #include "debug.h"
+#include "idt.h"
+#include "timer.h"
 
 int kern_entry()
 {
 	init_debug();
 	init_gdt();
+	init_idt();
 	console_clear();
 	//char str1[] = "Hello, OS Kernel";
 	//char str2[] = "Hello, Morain Kernel";
@@ -32,9 +34,20 @@ int kern_entry()
 	
 	//printk("strcmp(str1 , str2) = %d\n",strcmp(str1 , str2));
 	//print_cur_status();
-	panic("test");
+	//panic("test");
 
 	//print_cur_status();
 
+	//asm volatile ("int $0x3");
+	//asm volatile ("int $0x4");
+	
+	init_timer(2000);
+
+	//开启中断
+	asm volatile ("sti");
+	//asm volatile ("int $0x20");
+
+	//asm volatile("int $0x8");
+	//asm volatile("int $0xD");
 	return 0;
 }
